92fab4af941d3e80183cf1f8be3c95f7bff4bda7
Mete Ismayil <mismayilza@gmail.com>
2021-10-26 16:54:54 +0200

    Add least squares GD and SGD implementations

--- implementations.py
+++ implementations.py
@@ -46,53 +46,79 @@ def ridge_regression(y, tx, lambda_):
     N, D = tx.shape
     w = np.linalg.solve(np.dot(tx.T, tx) + 2 * lambda_ * N * np.eye(D), np.dot(tx.T, y))
     loss = compute_mse(y, tx, w)
-    return loss, w
+    return w, loss
 
 
-def build_poly(X, degree, cont_cols=None):
+def least_squares_gradient(y, tx, w):
+    return tx.T @ ((tx @ w) - y)
+
+
+def build_poly(X, degree, cont_features=None):
     X_poly = []
     for x in X:
         x_poly = []
         for d in range(1, degree+1):
-            x = x if cont_cols is None else x[cont_cols]
+            x = x if cont_features is None else x[cont_features]
             x_poly.append(x**d)
         X_poly.append(np.hstack(x_poly))
     return np.array(X_poly)
 
 
-def ridge_regression_cv(y, x, k_indices, k, lambda_, degree):
-    test_indices = k_indices[k]
-    train_indices = list(set(range(len(y))) - set(k_indices[k]))
-    train_x, train_y = x[train_indices], y[train_indices]
-    test_x, test_y = x[test_indices], y[test_indices]
+def least_squares_GD(y, tx, initial_w = None, max_iters = 100, gamma = 0.1, batch_size = None, num_batches = None, verbose = False):
+    losses = []
+    accuracies = []
+    max_accuracy = 0
+    best_epoch = None
+    loss = None
+    w = initial_w or np.random.rand(tx.shape[1], 1)
+    y = y.reshape((-1, 1))
 
-    # train_x_poly = build_poly(train_x, degree)
-    # test_x_poly = build_poly(test_x, degree)
+    for i in range(max_iters):
+        for tx_sample, y_sample in batch_iter(tx, y, batch_size=batch_size, num_batches=num_batches):
+            y_pred = predict_labels(w, tx_sample)
+            loss = compute_mse(y_sample, tx_sample, w)
+            accuracy = compute_accuracy(y_sample, y_pred)
+            w -= gamma * least_squares_gradient(tx, y, w)
+        
+        losses.append(loss)
+        accuracies.append(accuracy)
 
-    loss_tr, weights = ridge_regression(train_y, train_x, lambda_)
+        if accuracies[i] > max_accuracy:
+            max_accuracy = accuracies[i]
+            best_epoch = i
 
-    loss_te = compute_mse(test_y, test_x, weights)
-    y_pred = predict_labels(weights, test_x)
-    accuracy = compute_accuracy(test_y, y_pred)
-    f1 = compute_f1(test_y, y_pred)
-    return weights, loss_tr, loss_te, accuracy, f1
+        if verbose and i % 10 == 0:
+            print("Iteration {}/{}".format(i, max_iters))
+            print("Accuracy = {}%".format(accuracies[i]))
+            print("Loss = {}".format(loss))
+            print('\n')
+        
+    if verbose:
+        fig, axs = plt.subplots(1, 2)
+        axs[0].plot(list(range(max_iters)), accuracies)
+        axs[1].plot(list(range(max_iters)), losses)
+    
+    print("Best Accuracy : {}% reached at epoch {}".format(max_accuracy, best_epoch))
+    
+    return w, loss
+
+def least_squares_GD(y, tx, initial_w = None, max_iters = 100, gamma = 0.1, verbose = False):
+    return least_squares_GD(y, tx, initial_w=initial_w, max_iters=max_iters, gamma=gamma, verbose=verbose, batch_size=1)
 
 
-def build_k_indices(y, k_fold, seed):
-    """build k indices for k-fold."""
+def kfold_cv_iter(y, tx, k = 5, seed = 1):
     num_row = y.shape[0]
-    interval = int(num_row / k_fold)
+    fold_size = int(num_row / k)
     np.random.seed(seed)
     indices = np.random.permutation(num_row)
-    k_indices = [indices[k * interval: (k + 1) * interval]
-                 for k in range(k_fold)]
-    return np.array(k_indices)
+    
+    for i in range(k):
+        test_indices = indices[i * fold_size: (i + 1) * fold_size]
+        train_indices = list(set(range(num_row)) - set(test_indices))
+        yield tx[train_indices], y[train_indices], tx[test_indices], y[test_indices]
 
 
-def ridge_regression_kfold(y, tx, lambdas=np.logspace(-4, 0, 30), k_fold=4, degree=7, seed=1):
-    # split data in k fold
-    k_indices = build_k_indices(y, k_fold, seed)
-
+def ridge_regression_cv(y, tx, lambdas=np.logspace(-4, 0, 30), k_fold=4, degree=7, seed=1):
     best_lambda = None
     best_weights = None
     best_accuracy = None
@@ -103,8 +129,14 @@ def ridge_regression_kfold(y, tx, lambdas=np.logspace(-4, 0, 30), k_fold=4, degr
         rmse_losses = []
         accuracies = []
         f1s = []
-        for k in range(len(k_indices)):
-            weights, loss_tr, loss_te, accuracy, f1 = ridge_regression_cv(y, tx, k_indices, k, lambda_, degree)
+        for train_x, train_y, test_x, test_y in kfold_cv_iter(y, tx, k=k_fold, seed=seed):
+            weights, _ = ridge_regression(train_y, train_x, lambda_)
+
+            loss_te = compute_mse(test_y, test_x, weights)
+            y_pred = predict_labels(weights, test_x)
+            accuracy = compute_accuracy(test_y, y_pred)
+            f1 = compute_f1(test_y, y_pred)
+
             rmse_losses.append(np.sqrt(2 * loss_te))
             accuracies.append(accuracy)
             f1s.append(f1)
@@ -125,13 +157,20 @@ def standardize(x):
     return (x-np.mean(x, axis=0)) / np.std(x, axis=0)
 
 
-def get_batches(x, y, batch_size=None, num_batches=None):
-    batch_size = batch_size or len(x)
-    batches = int(np.ceil(len(x) // batch_size))
+def batch_iter(tx, y, batch_size=None, num_batches=None, shuffle=True):
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        y = y[shuffle_indices]
+        tx = tx[shuffle_indices]
+
+    batch_size = batch_size or len(tx)
+    batches = int(np.ceil(len(tx) // batch_size))
     num_batches = num_batches or batches
 
     for i in range(num_batches):
-        yield x[i*batch_size:(i+1)*batch_size], y[i*batch_size:(i+1)*batch_size]
+        yield tx[i*batch_size:(i+1)*batch_size], y[i*batch_size:(i+1)*batch_size]
 
 
 def split_data(x, y, ratio, seed=1):
@@ -143,7 +182,11 @@ def split_data(x, y, ratio, seed=1):
     return np.take(x, indexes[:split_i]), np.take(y, indexes[:split_i]), np.take(x, indexes[split_i:]), np.take(y, indexes[split_i:])
 
 
-def logistic_regression(y, tx, initial_w = None, max_iters = 1000, gamma = 0.1, lambda_ = 0, lr_decay = 0, verbose = False, batch_size=None, num_batches=None):
+def logistic_regression(y, tx, initial_w = None, max_iters = 1000, gamma = 0.1, lr_decay = 0, verbose = False, batch_size=None, num_batches=None):
+    return reg_logistic_regression(y, tx, lambda_=0, initial_w=initial_w, max_iters=max_iters, gamma=gamma, lr_decay=lr_decay, verbose=verbose, batch_size=batch_size, num_batches=num_batches)
+
+
+def reg_logistic_regression(y, tx, lambda_ = 0.1, initial_w = None, max_iters = 1000, gamma = 0.1, lr_decay = 0, verbose = False, batch_size=None, num_batches=None):
     losses = []
     accuracies = []
     max_accuracy = 0
@@ -153,9 +196,9 @@ def logistic_regression(y, tx, initial_w = None, max_iters = 1000, gamma = 0.1, 
     y = y.reshape((-1, 1))
 
     for i in range(max_iters):
-        for tx_sample, y_sample in get_batches(tx, y, batch_size=batch_size, num_batches=num_batches):
-            y_pred = predict(w, tx_sample)
-            loss = compute_binary_loss(y_sample, tx_sample, w)
+        for tx_sample, y_sample in batch_iter(tx, y, batch_size=batch_size, num_batches=num_batches):
+            y_pred = predict_log_reg(w, tx_sample)
+            loss = compute_log_loss(y_sample, tx_sample, w)
             accuracy = compute_accuracy(y_sample, y_pred)
             w -= gamma * logistic_gradient(tx, y, w, lambda_)
             gamma *= (1. / (1. + lr_decay*i))
@@ -180,13 +223,12 @@ def logistic_regression(y, tx, initial_w = None, max_iters = 1000, gamma = 0.1, 
     
     print("Best Accuracy : {}% reached at epoch {}".format(max_accuracy, best_epoch))
     
-    return loss, w
+    return w, loss
 
 
-def logistic_regression_cv(y, tx, lambdas=np.logspace(-4, 0, 5), degrees=list(range(1, 7)), k_fold=5, max_iters=100, gamma=0.1, degree=3, cont_cols=None, seed=1, verbose = False):
-    # split data in k fold
-    k_indices = build_k_indices(y, k_fold, seed)
-
+def logistic_regression_cv(y, tx, lambdas=None, degrees=None, k_fold=5, max_iters=100, gamma=0.1, degree=3, cont_features=None, seed=1, verbose = False):
+    lambdas = lambdas or np.logspace(-4, 0, 5)
+    degrees = degrees or list(range(1, 4))
     best_lambda = None
     best_degree = None
     best_weights = None
@@ -200,21 +242,16 @@ def logistic_regression_cv(y, tx, lambdas=np.logspace(-4, 0, 5), degrees=list(ra
     y = y.reshape((-1, 1))
 
     for degree in degrees:
-        tx_poly = build_poly(tx, degree, cont_cols)
+        tx_poly = build_poly(tx, degree, cont_features)
         for lambda_ in lambdas:
             losses = []
             accuracies = []
             f1s = []
-            for k in range(len(k_indices)):
-                test_indices = k_indices[k]
-                train_indices = list(set(range(len(y))) - set(k_indices[k]))
-                train_x, train_y = tx_poly[train_indices], y[train_indices]
-                test_x, test_y = tx_poly[test_indices], y[test_indices]
+            for train_x, train_y, test_x, test_y in kfold_cv_iter(y, tx_poly, k=k_fold, seed=seed):
+                weights, _ = reg_logistic_regression(train_y, train_x, max_iters=max_iters, lambda_=lambda_, gamma=gamma)
 
-                loss_tr, weights = logistic_regression(train_y, train_x, max_iters=max_iters, lambda_=lambda_, gamma=gamma, verbose=verbose)
-
-                loss_te = compute_binary_loss(test_y, test_x, weights)
-                y_pred = predict(weights, test_x)
+                loss_te = compute_log_loss(test_y, test_x, weights)
+                y_pred = predict_log_reg(weights, test_x)
                 accuracy = compute_accuracy(test_y, y_pred)
                 f1 = compute_f1(test_y, y_pred)
 
@@ -242,12 +279,13 @@ def logistic_regression_cv(y, tx, lambdas=np.logspace(-4, 0, 5), degrees=list(ra
                 print("Accuracy = {}%".format(accuracy))
                 print("F1 Score = {}".format(f1))
                 print("Loss = {}".format(loss))
+                print('\n')
     
     # if verbose:
     #     fig, axs = plt.subplots(1, 3, figsize=(16, 8))
     #     axs[0].plot(lambdas, cv_losses)
-    #     axs[1].plot(lambdas, lambda_accuracies)
-    #     axs[2].plot(lambdas, lambda_f1s)
+    #     axs[1].plot(lambdas, cv_accuracies)
+    #     axs[2].plot(lambdas, cv_f1s)
 
     return best_weights, min_loss, best_lambda, best_degree, best_accuracy, best_f1
 
@@ -260,22 +298,22 @@ def sigmoid(x):
     return np.exp(x) / (1 + np.exp(x))
 
 
-def predict(w, x):
+def predict_log_reg(w, x):
     y_pred = sigmoid(x @ w)
     y_pred[np.where(y_pred <= 0.5)] = 0
     y_pred[np.where(y_pred > 0.5)] = 1
     return y_pred
 
 
-def compute_binary_loss(y, x, w):
+def compute_log_loss(y, x, w):
     probs = sigmoid(x @ w)
     return -(1 / len(y)) * np.sum(y * np.log(probs) + (1-y) * np.log(1-probs))
 
 
 def switch_encoding(y):
-    y_copy = y.copy()
-    y_copy[np.where(y_copy == -1)] = 0
-    return y_copy
+    y = y.copy()
+    y[y == -1] = 0
+    return y
 
 
 def add_bias(x):
@@ -310,11 +348,11 @@ def transform_X(X, nan_cols, imputable_cols, encodable_cols):
     tX = add_bias(tX)
 
     # Get continous columns
-    cont_cols = list(range(tX.shape[1]))
+    cont_features = list(range(tX.shape[1]))
 
     tX = np.hstack([tX, encoded_X])
 
-    return tX, cont_cols
+    return tX, cont_features
 
 def transform_y(y):
     if y is not None:
@@ -334,7 +372,7 @@ def preprocess(X_train, y_train, X_test, y_test=None, imputable_threshold=0.5, e
     encodable_cols = (col_nan_ratio > encodable_threshold)
     
     # Transform train data
-    tX_train, cont_cols = transform_X(X_train, nan_cols=nan_cols, imputable_cols=imputable_cols, encodable_cols=encodable_cols)
+    tX_train, cont_features = transform_X(X_train, nan_cols=nan_cols, imputable_cols=imputable_cols, encodable_cols=encodable_cols)
 
     # Transform test data
     X_test = convert_nans(X_test)
@@ -344,7 +382,7 @@ def preprocess(X_train, y_train, X_test, y_test=None, imputable_threshold=0.5, e
     ty_train = transform_y(y_train)
     ty_test = transform_y(y_test)
 
-    return tX_train, ty_train, tX_test, ty_test, cont_cols
+    return tX_train, ty_train, tX_test, ty_test, cont_features
 
 
 def read_header(data_path):
